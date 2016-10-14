3efedcb6b12db9da5199051abc1d6b05a95f17af
Alex Krainov <ialexkrainov2@gmail.com>
2016-10-15 01:46:06 +0300

    .

---
+++ TravelBug/Controllers/Elements/SliderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelBug.Controllers.Elements
+{
+    public class SliderController : Controller
+    {
+
+        public ActionResult View()
+        {
+            return PartialView("View");
+        }
+    }
+}
